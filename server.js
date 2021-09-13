4df710aca8520f3f841e0e284c1f145c6b849a01
zaiddev1996 <82337416+zaiddev1996@users.noreply.github.com>
2021-09-13 13:42:49 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express"),
   app = express(),
-  port = process.env.PORT || 3000,
+  port = process.env.PORT || 8080,
   mongoose = require("mongoose"),
   Task = require("./api/models/wallpaperAppModel"), //created model loading here
   bodyParser = require("body-parser");
