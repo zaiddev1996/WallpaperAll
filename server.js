2104b288f4fdfc28e8a8180ae24759c27326b4ea
iamirtaza <abbasizaid1996@gmail.com>
2021-09-13 14:30:59 +0500

    sgsf

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var routes = require("./api/routes/wallpaperAppRoutes"); //importing route
 routes(app); //register the route
 
 // app.listen(port);
-app.listen(port, "0.0.0.0", function () {
+app.listen(port, "65.2.125.99", function () {
   console.log(`'Server listening at: ${port}`);
 });
 
